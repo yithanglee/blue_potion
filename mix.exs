b197f27bd1f96c4a1969909b475ca605c770df46
Damien <yithanglee@gmail.com>
2024-12-11 01:36:08 +0800

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BluePotion.MixProject do
   def project do
     [
       app: :blue_potion,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description,
